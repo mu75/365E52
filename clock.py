05a1dd8cf47fcdf8cd83755332449ba58251a98d
mu75 <104246676+mu75@users.noreply.github.com>
2023-10-07 20:33:32 +0800

    Create clock.py

---
+++ clock.py
@@ -0,0 +1,14 @@
+import time
+
+def focus_timer(minutes):
+    seconds = minutes * 60
+    while seconds > 0:
+        minutes, seconds = divmod(seconds, 60)
+        timer = '{:02d}:{:02d}'.format(minutes, seconds)
+        print(timer, end="\r")
+        time.sleep(1)
+        seconds -= 1
+    print("时间到！")
+
+# 设置专注时长为25分钟
+focus_timer(25)
